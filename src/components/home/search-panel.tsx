dc57bcb6db758159cc261d289798e2b2f3a54dea
Fugui <lacklock@gmail.com>
2026-09-14 11:55:32 +0800

    更新组件内容

--- src/components/home/search-panel.tsx
+++ src/components/home/search-panel.tsx
@@ -23,8 +23,8 @@ export function SearchPanel() {
           <Button type="button" size="lg">找岗位</Button>
         </div>
         <div className="popular-conditions">
-          <div><span className="condition-label">热门城市</span>{['北京', '上海', '广州'].map((city) => <button type="button" key={city}>{city}</button>)}</div>
-          <div><span className="condition-label">热门专业</span>{['计算机类', '电子信息类', '金融学类'].map((major) => <button type="button" key={major}>{major}</button>)}</div>
+          <div><span className="condition-label">热门城市</span>{['北京', '上海', '广州', '深圳', '杭州', '成都', '武汉', '南京', '西安', '重庆', '天津', '苏州', '青岛', '郑州', '长沙', '合肥'].map((city) => <button type="button" key={city}>{city}</button>)}</div>
+          <div><span className="condition-label">热门专业</span>{['计算机类', '电子信息类', '电气类', '机械类', '自动化类', '金融学类', '会计学', '法学类', '土木类', '材料类', '通信工程', '能源动力类'].map((major) => <button type="button" key={major}>{major}</button>)}</div>
         </div>
         <div className="direction-grid">
           {directions.map(({ label, icon: Icon }) => <Button variant="outline" type="button" key={label}><Icon data-icon="inline-start" />{label}</Button>)}
